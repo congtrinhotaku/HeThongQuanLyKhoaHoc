8c66ca683d1f4deda912f0eef9119d38481ff7b5
Phan Thanh Chien <chiendtr0@gmail.com>
2025-09-08 14:52:39 +0700

    them tinhtrang sv trong chitietbuoi

--- routes/admin/kioskRoutes.js
+++ routes/admin/kioskRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const kioskController = require("../../controllers/admin/kioskController");
 
+
 router.get("/", kioskController.getKiosk);
 router.post("/diemdanh", kioskController.postDiemDanh);
 
